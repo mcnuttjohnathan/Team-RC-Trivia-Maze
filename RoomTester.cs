99a1375816f279c365aa0ae96d0926b916c8cb25
zbaker <zcbaker@ymail.com>
2015-05-14 10:24:44 -0700

    Added Room Tester Class

---
+++ RoomTester.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class RoomTester
+{
+    public static void main(string[] args)
+    {
+        Room testRoom = new Room();
+        string testRoomStr = testRoom.tostring();
+    }
+}
