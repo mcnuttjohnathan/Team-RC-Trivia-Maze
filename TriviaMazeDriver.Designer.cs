4de504c724fa9d84f84138e29d2516cd2682ed19
mcnuttjohnathan <mcnuttjohnathan@gmail.com>
2015-05-14 20:54:07 -0700

    Removed sample SQLite code (I still have the code if anyone needs it as an example). added three buttons for each team member to use in order to demo their portion of the code.
    all none driver code should be packaged: com.teamrc.[package name] with whatever package you want for your code.

--- TriviaMazeDriver.Designer.cs
+++ TriviaMazeDriver.Designer.cs
@@ -24,21 +24,45 @@
         /// </summary>
         private void InitializeComponent() {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 113);
+            this.button1.Location = new System.Drawing.Point(67, 62);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(161, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Click me";
+            this.button1.Text = "Graphics/Movement Demo";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Maze Generation Demo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(67, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Database Demo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // TriviaMazeDriver
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "TriviaMazeDriver";
             this.ResumeLayout(false);
@@ -48,6 +72,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
