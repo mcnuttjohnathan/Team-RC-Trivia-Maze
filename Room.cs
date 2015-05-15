ed5484ecc3d449d2a6b94c4677c917e40375b8ed
zbaker <zcbaker@ymail.com>
2015-05-15 05:46:39 -0700

    Fixed Room, adding makeExits class to create path

--- Room.cs
+++ Room.cs
@@ -1,50 +1,76 @@
 ﻿using System;
 
-public class Room{
-    char[][] r;
-    int exits;
+namespace TriviaMaze
+{
+    public class Room
+    {
+        char[,] r;
+        int[] exits;
 
-    //DVC
-	public void Room(){
-        this.r = new char[5,5]{{'X','X','X','X','X'},
-                               {'X','O','O','O','X'},
-                               {'X','O','O','O','X'},
-                               {'X','O','O','O','X'},
-                               {'X','X','X','X','X'}};
-	}
-
-    //EVC
-    public void Room(int e){
-        this.r = new char[5,5]{{'X','X','X','X','X'},
-                               {'X','O','O','O','X'},
-                               {'X','O','O','O','X'},
-                               {'X','O','O','O','X'},
-                               {'X','X','X','X','X'}};
-
-        this.exits = e;
-        makeExits();
-    }
-
-    public int[] getExits(){
-        return this.e;
-    }
-
-    public void setExits(int[] e){
-        this.exits = e;
-    }
-
-    public string tostring(){
-        string result= "";
-        for(int i = 0; i < r.length; i++){
-            for(int j = 0; j < r[i].length; j++){
-                result += r[i][j];
-            }
-            result += "/n";
+        public Room()
+        {
+            this.r = new char[5, 5]{{'X','X','X','X','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','X','X','X','X'}};
         }
-        return result;
-    }
 
-    public {
+        public Room(int[] e)
+        {
+            this.r = new char[5, 5]{{'X','X','X','X','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','O','O','O','X'},
+                                   {'X','X','X','X','X'}};
 
+            this.exits = e;
+            makeExits();
+        }
+
+        public int[] getExits()
+        {
+            return this.exits;
+        }
+
+        public void setExits(int[] e)
+        {
+            this.exits = e;
+            makeExits();
+        }
+
+        public string tostring()
+        {
+            string result = "";
+            for (int i = 0; i < r.GetLength(0); i++)
+            {
+                for (int j = 0; j < r.GetLength(1); j++)
+                {
+                    result += r[i, j];
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+        public void makeExits()
+        {
+            if (this.exits[0] == 1)
+            {
+                this.r[0, 2] = 'O';
+            }
+            if (this.exits[1] == 1)
+            {
+                this.r[2, 4] = 'O';
+            }
+            if (this.exits[2] == 1)
+            {
+                this.r[4, 2] = 'O';
+            }
+            if (this.exits[3] == 1)
+            {
+                this.r[2, 0] = 'O';
+            }
+        }
     }
 }
