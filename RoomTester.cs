5c02299a4935961e956c8056d3e6fa44b8738b5d
zbaker <zcbaker@ymail.com>
2015-05-15 20:17:13 -0700

    Fixed the Tester class for the room/map

--- RoomTester.cs
+++ RoomTester.cs
@@ -7,32 +7,32 @@ namespace TriviaMaze
         static void Main(string[] args)
         {
             Room testRoom = new Room();
-            string testRoomStr = testRoom.tostring();
+            string testRoomStr = testRoom.toString();
             Console.WriteLine(testRoomStr);
 
             int[] a = { 1, 0, 0, 0 };
             Room testRoom2 = new Room(a);
-            string testRoomStr2 = testRoom2.tostring();
+            string testRoomStr2 = testRoom2.toString();
             Console.WriteLine(testRoomStr2);
 
             int[] b = { 1, 1, 0, 0 };
             Room testRoom3 = new Room(b);
-            string testRoomStr3 = testRoom3.tostring();
+            string testRoomStr3 = testRoom3.toString();
             Console.WriteLine(testRoomStr3);
 
             int[] c = { 1, 1, 1, 0 };
             Room testRoom4 = new Room(c);
-            string testRoomStr4 = testRoom4.tostring();
+            string testRoomStr4 = testRoom4.toString();
             Console.WriteLine(testRoomStr4);
 
             int[] d = { 1, 1, 1, 1 };
             Room testRoom5 = new Room(d);
-            string testRoomStr5 = testRoom5.tostring();
+            string testRoomStr5 = testRoom5.toString();
             Console.WriteLine(testRoomStr5);
 
             int[] e = { 1, 0, 0, 1 };
             Room testRoom6 = new Room(e);
-            string testRoomStr6 = testRoom6.tostring();
+            string testRoomStr6 = testRoom6.toString();
             Console.WriteLine(testRoomStr6);
 
             MazeGenerator mg = new MazeGenerator();
