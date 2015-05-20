fa9c94f9793d89618c82fa2337b83ff9ba822e09
zbaker <zcbaker@ymail.com>
2015-05-19 20:08:16 -0700

    Fixed Empty to be compatible with the coding standards

--- com/teamrc/gameobjects/Empty.cs
+++ com/teamrc/gameobjects/Empty.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/**
+ * Empty is the I_Collidable exteded component that represents the areas the main character cannot walk on, like walls
+ * 
+ * @author Zoe Baker
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -8,48 +14,70 @@ using System.Text;
 using System.Threading.Tasks;
 using TriviaMaze.com.teamrc.util;
 
-namespace TriviaMaze.com.teamrc.gameobjects
-{
-    public partial class Empty : Component, I_Collidable
-    {
+namespace TriviaMaze.com.teamrc.gameobjects{
+    public partial class Empty : Component, I_Collidable{
 
         private String type = CollisionManager.NONE;
         private String[] collisionTypes = {CollisionManager.NONE};
 
-        public Empty()
-        {
+        /**
+         * Main constructor for the Empty Component. 
+         * Nothing needs to be stored
+         **/
+        public Empty(){
             InitializeComponent();
         }
 
-        public Empty(IContainer container)
-        {
+        /**
+         * Default container constructor, will not be used
+         **/
+        public Empty(IContainer container){
             container.Add(this);
 
             InitializeComponent();
         }
 
-        public String toString()
-        {
+        /**
+         * The toString used for printing and testing purposes
+         * 
+         * @returns x - the string denoting a wall
+         **/
+        public String toString(){
             return "X";
         }
 
-        public String getType()
-        {
+        /**
+         * The overrided get type method
+         * 
+         * @returns string - denoting the type
+         **/
+        public String getType(){
             return this.type;
         }
         
-        public String[] getCollisionTypes()
-        {
+        /**
+         * The overrided get collision type
+         * 
+         * @returns String[] - denoting the collision type is empty
+         **/
+        public String[] getCollisionTypes(){
             return this.collisionTypes;
         }
 
-        public Point getPosition()
-        {
+        /**
+         * The overrride get posion 
+         * 
+         * @returns Point - a random point of no value for an empty position
+         **/
+        public Point getPosition(){
             return new Point(-1, -1);
         }
 
-        public void collidedWith(I_Collidable c)
-        {
+        /** The overriden colllided with method
+         * 
+         * @param c - the I_Collidable object it could be colliding with
+         **/
+        public void collidedWith(I_Collidable c){
             
         }
     }
